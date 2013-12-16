b94f1df2db1a806d371f6332621651ef979306f0
dharmababa <cmorris@outlook.com>
2013-12-15 19:04:47 -0800

    Revert "Added social widgets using addthis.com"
    This reverts commit c8bb3f66c8d386cf0986ebed5359f504ee463b51.

--- wp-content/themes/Karma/footer.php
+++ wp-content/themes/Karma/footer.php
@@ -68,24 +68,5 @@ if(has_nav_menu('Footer Navigation')): ?>
 //codes to load scripts has been moved to truethemes_framework/global/javascript.php
 wp_footer();
 ?>
-<!-- AddThis Smart Layers BEGIN -->
-<!-- Go to http://www.addthis.com/get/smart-layers to customize -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52ab550d7d9d8966"></script>
-<script type="text/javascript">
-  addthis.layers({
-    'theme' : 'transparent',
-    'share' : {
-      'position' : 'left',
-      'services' : 'facebook,twitter,mailto'
-    }, 
-    'follow' : {
-      'services' : [
-        {'service': 'facebook', 'id': 'KMCWA'}
-      ]
-    },
-    'thankyou' : false
-  });
-</script>
-<!-- AddThis Smart Layers END -->
 </body>
 </html>
