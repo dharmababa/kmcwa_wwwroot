f8aaf7eec9c569c79316600109b85b18330c7167
dharmababa <cmorris@outlook.com>
2014-01-18 19:57:33 -0800

    Migration to InMotion

--- wp-config-azure.php
+++ wp-config-azure.php
@@ -14,21 +14,18 @@
  * @package WordPress
  */
 
-define('WP_HOME','http://biz123.inmotionhosting.com/~nktkmc5'); 
-define('WP_SITEURL','http://biz123.inmotionhosting.com/~nktkmc5');
-
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'nktkmc5_kmcwawp');
+define('DB_NAME', 'kmcwaAEI497Jfro1');
 
 /** MySQL database username */
-define('DB_USER', 'nktkmc5_kmcwawp');
+define('DB_USER', 'bda72fb655a3d5');
 
 /** MySQL database password */
-define('DB_PASSWORD', '(T5S0PZd!1');
+define('DB_PASSWORD', 'db5667a1');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-azure-west-b.cleardb.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -45,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'wbzmkauhta2fphjge6ssnxdvbvmbmepasdunyvwqi8btaje63v9aklitqhlmfctu');
-define('SECURE_AUTH_KEY',  'sf50kszew20tt8u2ljr5angmdvvypwsw35mtjkesayaxyxgoqzqkmz0ctmgoykpv');
-define('LOGGED_IN_KEY',    '8san3cxkvk3goqvx5xr0wuaerbu4coobhomo3x6uguduinfj7w7i20ekkechyhle');
-define('NONCE_KEY',        'ovgzy4zbjjio6vtypg5aprdkhrmah7d2uyuny4awkegugq9j5pqcuyy15hwglr3q');
-define('AUTH_SALT',        'wanoqwt5ecu7sa1a9qjwyjqemglvfxfvscpedrfgmy4qjpnlivkiyvhqt1ts5ddj');
-define('SECURE_AUTH_SALT', 'kduk6eqiuunncebhmji5gtp5wsftwq3unjfifppuh3wxddvxzkex635dorgpzbda');
-define('LOGGED_IN_SALT',   'lhcocanmfmvqkbjigeisb1ualrcdoh7yeeggwqlrw7awn0nefmykai739lyrfliw');
-define('NONCE_SALT',       'osxenoqyiis9v7eirylm5zafsfbrei1xioruqi9jkfxv1qk2fkvhvgdzmwyoef1p');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
