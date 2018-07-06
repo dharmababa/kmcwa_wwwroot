cbbb65e3c25ed18ab228b91676dc420450999007
Charles Morris <cmorris@microsoft.com>
2018-07-06 14:03:22 -0700

    Initial child theme for The7

---
+++ wp-content/themes/dt-the7-child/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+/*
+ * Adds start time to event titles in Month view
+ */
+function tribe_add_start_time_to_event_title ( $post_title, $post_id ) {
+ 
+    if ( !tribe_is_event($post_id) ) return $post_title;
+    // Checks if it is the month view, modify this line to apply to more views
+    if ( !tribe_is_month() ) return $post_title;
+ 
+    $event_start_time = tribe_get_start_time( $post_id );
+ 
+    if ( !empty( $event_start_time ) ) {
+        $post_title = $event_start_time . ' - ' . $post_title;
+    }
+ 
+    return $post_title;
+}
+add_filter( 'the_title', 'tribe_add_start_time_to_event_title', 100, 2 );
+
+?>
